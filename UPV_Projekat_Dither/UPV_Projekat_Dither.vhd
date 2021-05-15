8934e4b1d9f9c3017c6c9bb2625847b41bb827d6
Lazar Janicijevic <lazar.janicijevic@novelic.com>
2021-05-15 17:12:15 +0200

    Removed unnecessary instances for SDRAM testing

--- UPV_Projekat_Dither/UPV_Projekat_Dither.vhd
+++ UPV_Projekat_Dither/UPV_Projekat_Dither.vhd
@@ -143,62 +143,6 @@ component pll is
 end component;
 
 
-component cam_ctr is
-	port
-	(
-		reset : in std_logic;
-		clk : in std_logic;
-		S_C, S_D : out std_logic;
-		busy : out std_logic
-	);
-end component;
-
-component cam_read is
-	port
-	(
-		reset : in	std_logic;
-		Pclk, Href, Vsync	: in  std_logic;
-		data : in std_logic_vector (7 downto 0);
-		R,G,B	: out std_logic_vector (4 downto 0);
-		valid: out std_logic := '0';
-		x	:out std_logic_vector (9 downto 0);
-		y 	:out std_logic_vector (8 downto 0)
-	);
-end component;
-
-component dither_engine is
-	generic
-	(
-		CAM_WIDTH : integer
-	);
-	port 
-	(
-		reset : in std_logic;
-		clk	: in std_logic;
-		valid	: in std_logic;
-		Vsync	: in std_logic;
-		rin, gin, bin	: in std_logic_vector(4 downto 0);
-		rgb_out	: out std_logic_vector(2 downto 0); --rgb
-		w_e	: out std_logic := '0';
-		adr	: out std_logic_vector(18 downto 0)
-	);
-end component;
-
-component write_interface is
-	port
-	(
-		din : in std_logic_vector(2 downto 0);
-		adr : in std_logic_vector(18 downto 0); --307200 pix -> 19 bits needed
-		w_e : in std_logic;
-		vsync:in std_logic;
-		clk : in std_logic;
-		finished_frame : out std_logic_vector(1 downto 0);
-		reset : in std_logic;
-		address_out:	out std_logic_vector(21 downto 0);
-		data_out : out std_logic_vector(15 downto 0)
- 	);
-end component;
-
 component SDRAM_control is
 	port
 	(
@@ -223,65 +167,6 @@ component SDRAM_control is
 	);
 end component;
 
-component read_interface is
-	port
-	(
-		dout : out std_logic_vector(2 downto 0);
-		adr : in std_logic_vector(18 downto 0); --307200 pix -> 19 bits needed
-		finished_frame : in std_logic_vector(1 downto 0);
-		vsync:in std_logic;		--carefull, vsync is inverted here, pulses are 0
-		clk : in std_logic;
-		reset : in std_logic;
-		address_out:	out std_logic_vector(21 downto 0);
-		data_in : in std_logic_vector(15 downto 0)
- 	);
-end component;
-
-component vga_interface is
-	generic (
-		H_DISPLAY: integer := 1024;	-- visible pixels
-		V_DISPLAY: integer := 768;		-- visible pixels
-		CAM_WIDTH : integer := 640;
-		CAM_HEIGHT : integer := 480
-	);
-	port
-	(
-		clk : in std_logic;
-		reset : in std_logic;
-		hpos : in integer range 0 to H_DISPLAY-1;
-		vpos : in integer range 0 to V_DISPLAY-1;
-		vsync : in std_logic;
-		din : in std_logic_vector(2 downto 0);
-		dout : out std_logic_vector(2 downto 0);
-		adr : out std_logic_vector(18 downto 0)
- 	);
-end component;
-
-component vga_sync is
-	generic (
-		-- Default display mode is 1024x768@60Hz -- 60MHz clk
-		-- Horizontal line
-		H_SYNC	: integer := 136;		-- sync pulse in pixels
-		H_BP		: integer := 160;		-- back porch in pixels
-		H_FP		: integer := 24;		-- front porch in pixels
-		H_DISPLAY: integer := 1024;	-- visible pixels
-		-- Vertical line
-		V_SYNC	: integer := 6;		-- sync pulse in pixels
-		V_BP		: integer := 29;		-- back porch in pixels
-		V_FP		: integer := 3;		-- front porch in pixels
-		V_DISPLAY: integer := 768		-- visible pixels
-	);
-	port (
-		clk : in std_logic;
-		reset : in std_logic;
-		hsync, vsync : out std_logic;
-		sync_n, blank_n : out std_logic;
-		hpos : out integer range 0 to H_DISPLAY - 1;
-		vpos : out integer range 0 to V_DISPLAY - 1;
-		Rin, Gin, Bin : in std_logic;
-		Rout, Gout, Bout : out std_logic
-	);
-end component;
 
 --CONSTANTS
 constant H_DISPLAY: integer := 1024;
@@ -293,7 +178,6 @@ constant CAM_HEIGHT : integer := 480;
 signal reset : std_logic;
 	--pll
 signal c_sccb : std_logic;
-signal c_vga : std_logic;	--c_vga
 signal c_sdram_int : std_logic;
 	--cam_ctr
 signal cam_busy : std_logic;	--busy
@@ -343,7 +227,6 @@ begin
 
 	reset <= not reset_n;
 
-	vga_vsync <= vga_vsync_int;	--Zbog problema sa citanjem izlaznog porta
 	c_sdram <= c_sdram_int;
 	--PAZI,  MOZDA JE RESET INVERTOVAN
 	
@@ -395,67 +278,11 @@ c_sdram_int <= clk_50;
 pll_inst:  pll
 	port map (  
 		inclk0	=> clk_50,
-		c0 => c_vga, 
+		c0 => OPEN, --VGA clk 
 		c1	=> OPEN, --Testing SDRAM with 50 MHZ
-		c2 => c_cam ,
-		c3 => c_sccb
+		c2 => OPEN , --CAM clk
+		c3 => c_sccb --CAM sccb clk
 	);
-	
-
-cam_ctr_inst:  cam_ctr
-	port map (  
-		reset => reset,
-		clk => c_sccb,
-		S_C => S_C, 
-		S_D => S_D,
-		busy => cam_busy
-	);
-
-cam_read_inst:  cam_read
-	port map (  
-		reset => cam_busy,
-		Pclk => pclk,
-		Href => cam_href,
-		Vsync => cam_vsync,
-		data => cam_data,
-		R => R_cam,
-		G => G_cam,
-		B => B_cam,
-		valid => cam_data_valid,
-		x => x_cam, 
-		y => OPEN
-	);
-
-dither_engine_inst:  dither_engine
-	generic map (  
-		CAM_WIDTH => CAM_WIDTH
-	)
-	port map (  
-		reset => cam_busy,
-		clk	=> pclk,
-		valid => cam_data_valid,
-		Vsync => cam_vsync,
-		rin => R_cam,
-		gin => G_cam,
-		bin => B_cam,
-		rgb_out	=> RGB_dither_write,
-		w_e	=> write_pixel,
-		adr	=> write_adr
-	);
-
-
-write_interface_inst:  write_interface
-	port map (  
-		din => "101",		-- TODOTODO (write_adr(2),write_adr(2),write_adr(2) ),					-- TODO changed    RGB_dither_write,
-		adr => write_adr,
-		w_e => write_pixel,
-		vsync => cam_vsync,
-		clk => pclk,
-		finished_frame => finished_frame,
-		reset => cam_busy,
-		address_out =>  OPEN , 		--TODO EXTREME  vrati ovo 							s_ctr_wr_adr,
-		data_out => OPEN 				--TODO EXTREME   vrati ovo, mem debug test 	s_ctr_wr_data
- 	);
 	
 --	
 --	--TODO testiranje
@@ -527,56 +354,16 @@ SDRAM_control_inst:  SDRAM_control
 		clk_en => sd_cen
 	);
 
-read_interface_inst:  read_interface
-	port map (  
-		dout => RGB_dither_read,
-		adr => read_adr,
-		finished_frame => finished_frame,
-		vsync => vga_vsync_int,
-		clk => c_sdram_int,
-		reset => reset,
-		address_out => OPEN,			--TODO EXTREME vrati   s_ctr_rd_adr,
-		data_in => (others => '1')			--TODO EXTREME vrati    s_ctr_rd_data
- 	);
+	
+	
+	c_cam <= 'Z';
+	S_C <= 'Z';	
+	S_D <= 'Z';			
+	vga_hsync <= 'Z';
+	vga_vsync <= 'Z';
+	vga_r <= 'Z';	
+	vga_g <= 'Z';
+	vga_b <= 'Z';
 
-vga_interface_inst:  vga_interface
-	generic map (
-		H_DISPLAY => H_DISPLAY,
-		V_DISPLAY => V_DISPLAY,
-		CAM_WIDTH => CAM_WIDTH,
-		CAM_HEIGHT => CAM_HEIGHT 
-	)
-	port map (  
-		clk => c_vga,
-		reset => reset,
-		hpos => vga_x,
-		vpos => vga_y,
-		vsync => vga_vsync_int,
-		din => RGB_dither_read,
-		dout => vga_data,
-		adr => OPEN    --TODO read_adr bilo ovde, otkaceno
- 	);
-
-vga_sync_inst:  vga_sync
-	generic map (
-		H_DISPLAY => H_DISPLAY,
-		V_DISPLAY => V_DISPLAY
-	)
-	port map (  
-		clk => c_vga,
-		reset => reset,
-		hsync => vga_hsync,
-		vsync => vga_vsync_int,
-		sync_n => OPEN,
-		blank_n => OPEN,
-		hpos => vga_x,
-		vpos => vga_y,
-		Rin => vga_data(2),
-		Gin => vga_data(1),
-		Bin => vga_data(0),
-		Rout => vga_r,
-		Gout => vga_g,
-		Bout => vga_b
-	);
 
 end struct;
